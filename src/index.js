cc1dd5fa4f6bd3cde7b4b6157232dee42cb37610
Himanshu <himanshu@zethic.com>
2021-09-06 18:02:05 +0530

    add autolaunch app capability

--- src/index.js
+++ src/index.js
@@ -1,46 +1,55 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, Menu, Tray } = require("electron");
+const AutoLaunch = require("auto-launch");
+const unhandled = require("electron-unhandled");
+const path = require("path");
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+unhandled();
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+const autoLauncher = new AutoLaunch({
+  name: "Saba Proxy",
+});
+
+autoLauncher
+  .isEnabled()
+  .then((isEnabled) => {
+    if (isEnabled) {
+      return;
+    }
+    autoLauncher.enable();
+  })
+  .catch((err) => {
+    throw err;
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+let tray = null;
+app.whenReady().then(() => {
+  tray = new Tray(path.join(__dirname, "assets/avatar.png"));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Item1",
+      click: function () {
+        console.log("Clicked on settings");
+      },
+    },
+    {
+      label: "Item2",
+      click: function () {
+        console.log("Clicked on settings");
+      },
+    },
+    {
+      label: "Item3",
+      click: function () {
+        console.log("Clicked on settings");
+      },
+    },
+    {
+      label: "Exit",
+      click: function () {
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("This is my application.");
+  tray.setContextMenu(contextMenu);
 });
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
