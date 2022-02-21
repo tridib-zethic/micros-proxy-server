66a1941a3a4212154c8b90cd5c8d29f0ae36700d
Tridib Dawn <tridib@zethic.com>
2022-02-21 16:49:57 +0530

    update login form

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,14 +1,3 @@
-// const { ipcRenderer } = require("electron");
-// document.getElementById("submit").onclick = function () {
-//   const username = document.getElementById("username").value;
-//   const password = document.getElementById("password").value;
-//   console.log(
-//     ipcRenderer.sendSync("login_submit", {
-//       username,
-//       password,
-//     })
-//   );
-// };
 const { ipcRenderer } = require("electron");
 const psl = require("psl");
 
